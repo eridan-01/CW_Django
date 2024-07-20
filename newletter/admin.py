336cd0ca8ffd54086a506efc94927a9370b33d70
Eugene Geym <geymzheka@gmail.com>
2024-07-21 01:58:39 +0500

    added admin register

--- newletter/admin.py
+++ newletter/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Client, Message, Mailing, Attempt
 
-# Register your models here.
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('email', 'last_name', 'first_name', 'middle_name', 'comment')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'body')
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('start_time', 'periodicity', 'status', 'message')
+
+
+@admin.register(Attempt)
+class AttemptAdmin(admin.ModelAdmin):
+    list_display = ('mailing', 'attempt_time', 'status', 'server_response')
+
