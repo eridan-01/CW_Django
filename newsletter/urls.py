6b12e2cfef9337f67147421874dd4304220499ca
Eugene Geym <geymzheka@gmail.com>
2024-07-26 00:31:09 +0500

    added urls for clients

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -1,8 +1,14 @@
-from newsletter.apps import NewsletterConfig
+from django.urls import path
 
+from newsletter.apps import NewsletterConfig
+from newsletter.views import ClientListView, ClientDetailView, ClientCreateView, ClientUpdateView, ClientDeleteView
 
 app_name = NewsletterConfig.name
 
 urlpatterns = [
-
+    path('clients/', ClientListView.as_view(), name='client-list'),
+    path('clients/<int:pk>/', (ClientDetailView.as_view()), name='client-detail'),
+    path('clients/create/', ClientCreateView.as_view(), name='client-create'),
+    path('clients/<int:pk>/update/', ClientUpdateView.as_view(), name='client-update'),
+    path('clients/<int:pk>/delete/', ClientDeleteView.as_view(), name='client-delete'),
 ]
