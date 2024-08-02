3c3b435bc6e7e4f51cd3da1b5b260b2492492bdf
Eugene Geym <geymzheka@gmail.com>
2024-08-02 14:55:13 +0500

    added blog model and admin register

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from blog.models import Blog
 
-# Register your models here.
+
+@admin.register(Blog)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publication_date', 'views')
+    search_fields = ('title', 'content')
+    list_filter = ('publication_date',)
+    ordering = ('-publication_date',)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Содержимое статьи')
+    image = models.ImageField(upload_to='blog_images/', null=True, blank=True, verbose_name='Изображение')
+    views = models.PositiveIntegerField(default=0, verbose_name='Количество просмотров')
+    publication_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Блога'
+        verbose_name_plural = 'Блоги'
