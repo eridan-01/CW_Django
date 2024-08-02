f600e6a9bb4cc585e619e1e876123caceea550ce
Eugene Geym <geymzheka@gmail.com>
2024-08-02 14:20:33 +0500

    added url and user list

--- users/urls.py
+++ users/urls.py
@@ -2,11 +2,12 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UserCreateView, email_verification, PasswordResetView
+from users.views import UserCreateView, email_verification, PasswordResetView, UserListView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
+    path('user_list/', UserListView.as_view(template_name='user_list.html'), name='user_list'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/', UserCreateView.as_view(), name='register'),
